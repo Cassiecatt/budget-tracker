f32a9b43eb3a19d02f81ed281c7f65de89fc8d09
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-03 13:35:42 -0700

    added on success and on error functions

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,7 +6,22 @@ const request = indexedDB.open("budget", 1);
 //this event will emit if the DB version changes
 request.onupgradeneeded = function (e) {
   //save a reference to the DB
-  const db = e.target.result;
+  let db = e.target.result;
   //create an object store (table)
-  db.createObjectStore("new_budget", { autoIncrement: true });
+  db.createObjectStore("newBudget", { autoIncrement: true });
+};
+
+//successful
+request.onsuccess = function (e) {
+  //when db is successfully created with its object store - save reference to db in global variable
+  db = e.target.result;
+
+  //check if app is online
+  if (navigator.onLine) {
+    // checkDatabase();
+  }
+};
+
+request.onerror = function (e) {
+  console.log(e.target.errorCode);
 };
