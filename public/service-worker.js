49d4267b211457dadd86cdf8703ad2510e4b7902
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-03 13:04:32 -0700

    fixed error in icon path

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
   "/index.html",
   "/manifest.json",
   "/css/styles.css",
-  "/icons/icon-72x72x.png",
+  "/icons/icon-72x72.png",
   "/icons/icon-96x96.png",
   "/icons/icon-128x128.png",
   "/icons/icon-144x144.png",
